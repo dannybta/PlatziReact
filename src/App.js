9c1b9e4d97ed57982df9182c7ced89c6168e9777
Daniels <dannybm19@gmail.com>
2022-03-21 21:50:25 -0500

    Intro react Clase 5

---
+++ src/App.js
@@ -0,0 +1,26 @@
+// import './App.css';
+import React from 'react';
+import { TodoCounter } from './TodoCounter';
+import { TodoSearch } from './TodoSearch';
+import { TodoList } from './TodoList';
+import { TodoItem } from './TodoItem';
+import { CreateTodoButton } from './CreateTodoButton';
+const todos=[
+  {text:'Cortar Cebolla',completed:false},
+  {text:'Tomar el curso de intro a React',completed:false},
+  {text:'Llorar con la llorona',completed:false}
+]
+function App(props) {
+  return (
+  <React.Fragment>
+    <TodoCounter />
+  <TodoSearch/>    
+    <TodoList>
+      {todos.map(todo=>(<TodoItem key={todo.text} text={todo.text}/>))}
+    </TodoList>
+    <CreateTodoButton/>    
+  </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/TodoCounter.js
@@ -0,0 +1,10 @@
+
+import React from 'react';
+import './TodoCounter.css';
+
+function TodoCounter(){
+
+    return (<h2 className="TodoCounter">Has Completado 2 de 3 to do's</h2>)
+}
+
+export {TodoCounter}
